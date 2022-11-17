ac4a8690a6a3706e403e9aad59cdb7d80f751e60
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2022-11-17 10:27:21 +0530

    just check

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Satendra Singh Gurjar!
         </a>
       </header>
     </div>
