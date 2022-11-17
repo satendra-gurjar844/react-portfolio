95aabce059b8a6d73d5b9307e006c3984a51a7ff
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2022-11-17 12:11:56 +0530

    changes uplaod

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
 import Footer from "./Component/Footer";
 import Header from "./Component/Header";
+import Portfolio from "./Component/Portfolio";
 import Profile from "./Component/Profile";
+import Resume from "./Component/Resume";
+
 
 function App() {
   return (
   <>
     <Header/>
+
     <Profile/>
+    <Portfolio/>
+    <Resume/>
+    <About/>
+    <Contact/>
+    
     <Footer/>
   </>
   );
---
+++ my-portfolio/src/Component/Contact.jsx
@@ -0,0 +1,239 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <><section class="site-section">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md-12">
+                <div class="section-heading text-center">
+                    <h2>Client <strong>Testimonial</strong></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+
+                <div class="block-47 d-flex mb-5">
+                    <div class="block-47-image">
+                        <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid"/>
+                    </div>
+                    <blockquote class="block-47-quote">
+                        <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+                        <cite class="block-47-quote-author">&mdash; Ethan McCown, CEO <a href="#">XYZ Inc.</a></cite>
+                    </blockquote>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="block-47 d-flex mb-5">
+                    <div class="block-47-image">
+                        <img src="images/person_2.jpg" alt="Image placeholder" class="img-fluid"/>
+                    </div>
+                    <blockquote class="block-47-quote">
+                        <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+                        <cite class="block-47-quote-author">&mdash; Craig Gowen, CEO <a href="#">XYZ Inc.</a></cite>
+                    </blockquote>
+                </div>
+
+            </div>
+
+            <div class="col-md-6">
+
+                <div class="block-47 d-flex mb-5">
+                    <div class="block-47-image">
+                        <img src="images/person_3.jpg" alt="Image placeholder" class="img-fluid"/>
+                    </div>
+                    <blockquote class="block-47-quote">
+                        <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+                        <cite class="block-47-quote-author">&mdash; Ethan McCown, CEO <a href="#">XYZ Inc.</a></cite>
+                    </blockquote>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="block-47 d-flex mb-5">
+                    <div class="block-47-image">
+                        <img src="images/person_4.jpg" alt="Image placeholder" class="img-fluid"/>
+                    </div>
+                    <blockquote class="block-47-quote">
+                        <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+                        <cite class="block-47-quote-author">&mdash; Craig Gowen, CEO <a href="#">XYZ Inc.</a></cite>
+                    </blockquote>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<section class="site-section pb-0"  id="section-services">
+    <div class="container">
+
+        <div class="row mb-4">
+            <div class="col-md-12">
+                <div class="section-heading text-center">
+                    <h2>My <strong>Services</strong></h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-md-6 col-lg-4 text-center mb-5">
+                <div class="site-service-item site-animate" data-animate-effect="fadeIn">
+                    <span class="icon">
+                        <span class="icon-browser2"></span>
+                    </span>
+                    <h3 class="mb-4">Web Design</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                    <p><a href="#" class="site-link">Learn More <i class="icon-chevron-right"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 text-center mb-5">
+                <div class="site-service-item site-animate" data-animate-effect="fadeIn">
+                    <span class="icon">
+                        <span class="icon-presentation"></span>
+                    </span>
+                    <h3 class="mb-4">Search Engine Optimization</h3>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                    <p><a href="#" class="site-link">Learn More <i class="icon-chevron-right"></i></a></p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4 text-center mb-5">
+                <div class="site-service-item site-animate" data-animate-effect="fadeIn">
+                    <span class="icon">
+                        <span class="icon-video2"></span>
+                    </span>
+                    <h3 class="mb-4">Video Editing</h3>
+                    <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+                    <p><a href="#" class="site-link">Learn More <i class="icon-chevron-right"></i></a></p>
+                </div>
+            </div>
+
+            
+
+        </div>
+    </div>
+</section>
+
+
+
+
+<section class="site-section" id="section-blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mb-5">
+                <div class="section-heading text-center">
+                    <h2>Blog on <strong>Medium</strong></h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="blog-entry">
+                    <a href="#"><img src="images/post_1.jpg" alt="Image placeholder" class="img-fluid"/></a>
+                    <div class="blog-entry-text">
+                        <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                        <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+
+                        <div class="meta">
+                            <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                            <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="blog-entry">
+                    <a href="#"><img src="images/post_2.jpg" alt="Image placeholder" class="img-fluid"/></a>
+                    <div class="blog-entry-text">
+                        <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                        <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+
+                        <div class="meta">
+                            <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                            <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="blog-entry">
+                    <a href="#"><img src="images/post_3.jpg" alt="Image placeholder" class="img-fluid"/></a>
+                    <div class="blog-entry-text">
+                        <h3><a href="#">Creative Product Designer From Facebook</a></h3>
+                        <p class="mb-4">Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+
+                        <div class="meta">
+                            <a href="#"><span class="icon-calendar"></span> Aug 7, 2018</a>
+                            <a href="#"><span class="icon-bubble"></span> 5 Comments</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="site-section" id="section-contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mb-5">
+                <div class="section-heading text-center">
+                    <h2>Get <strong>In Touch</strong></h2>
+                </div>
+            </div>
+
+            <div class="col-md-7 mb-5 mb-md-0">
+                <form action="" class="site-form">
+                    <h3 class="mb-5">Get In Touch</h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control px-3 py-4" placeholder="Your Name"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control px-3 py-4" placeholder="Your Email"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control px-3 py-4" placeholder="Your Phone"/>
+                    </div>
+                    <div class="form-group mb-5">
+                        <textarea class="form-control px-3 py-4"cols="30" rows="10" placeholder="Write a Message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary  px-4 py-3" value="Send Message"/>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-5 pl-md-5">
+                <h3 class="mb-5">My Contact Details</h3>
+                <ul class="site-contact-details">
+                    <li>
+                        <span class="text-uppercase">Email</span>
+                        satendragurjar844@gmail.com
+                    </li>
+                    <li>
+                        <span class="text-uppercase">Phone</span>
+                        +91-9873313136
+                    </li>
+                    <li>
+                        <span class="text-uppercase">Fax</span>
+                        +91-9873313136
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+</section></>
+  )
+}
+
+export default Contact
